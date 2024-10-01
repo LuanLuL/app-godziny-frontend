23a1120d2a2f0c342b75c2f8c6128761feab04ba
Luan Gonçalves Santos <luanlulu2010@hotmail.com>
2024-10-01 00:10:22 -0300

    Adding axios and doing login

--- src/ui/sysPages/example/Example.tsx
+++ src/ui/sysPages/example/Example.tsx
@@ -1,43 +1,78 @@
 import React from "react";
 import { Box, Button, Typography } from "@mui/material";
 import { SysAppLayoutContext } from "../../../app/AppLayout";
+import axios, { AxiosError } from "axios";
 
 export const Example = () => {
   const { showNotification } = React.useContext(SysAppLayoutContext);
 
   return (
-    <Box sx={{ padding: "40px" }}>
+    <Box
+      sx={{
+        padding: "40px",
+        display: "flex",
+        gap: "2rem",
+        flexDirection: "column",
+      }}
+    >
       <Typography>EXAMPLE PAGE</Typography>
-      <Button
-        onClick={() =>
-          showNotification({
-            message: "Operação realizada com sucesso!",
-            type: "success",
-          })
-        }
-      >
-        Success Notification
-      </Button>
-      <Button
-        onClick={() =>
-          showNotification({
-            message: "Operação está com aviso!",
-            type: "warning",
-          })
-        }
-      >
-        Warning Notification
-      </Button>
-      <Button
-        onClick={() =>
-          showNotification({
-            message: "Operação incompleta por causa de algum erro!",
-            type: "error",
-          })
-        }
-      >
-        Error Notification
-      </Button>
+      <Box>
+        <Button
+          onClick={() =>
+            showNotification({
+              message: "Operação realizada com sucesso!",
+              type: "success",
+            })
+          }
+        >
+          Success Notification
+        </Button>
+        <Button
+          onClick={() =>
+            showNotification({
+              message: "Operação está com aviso!",
+              type: "warning",
+            })
+          }
+        >
+          Warning Notification
+        </Button>
+        <Button
+          onClick={() =>
+            showNotification({
+              message: "Operação incompleta por causa de algum erro!",
+              type: "error",
+            })
+          }
+        >
+          Error Notification
+        </Button>
+      </Box>
+      <Box>
+        <Button onClick={() => handleLogin()}>Login</Button>
+      </Box>
     </Box>
   );
+
+  async function handleLogin() {
+    try {
+      console.log("entrou");
+      const response = await axios.post("http://localhost:8080/auth/login", {
+        email: "adm@uemg.com",
+        senha: "123456",
+      });
+      console.log("LOGIN  = ", response);
+      showNotification({
+        type: "success",
+        message: "Login realizado com sucesso!",
+      });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        showNotification({
+          type: "error",
+          message: error.response?.data?.detail || "An unknown error occurred",
+        });
+      }
+    }
+  }
 };
